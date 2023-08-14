6e2933769835387bc4c48fc2978f01e9798784f1
Sungchan Yi <calofmijuck@snu.ac.kr>
2023-08-14 13:52:08 +0900

    feat: added more set lemmas (from high school books)

--- basics/Sets.v
+++ basics/Sets.v
@@ -56,6 +56,7 @@ End Sets.
 
 Arguments element {U}.
 Arguments empty_set {U}.
+Arguments full_set {U}.
 Arguments subset {U}.
 Arguments intersection {U}.
 Arguments union {U}.
@@ -71,6 +72,7 @@ Infix "⋃" := union (left associativity, at level 45).
 Infix "\" := difference (left associativity, at level 43).
 Infix "△" := symmetric_difference (at level 45, left associativity).
 
+Global Hint Constructors full_set : core.
 Global Hint Constructors intersection : core.
 Global Hint Constructors union : core.
 Global Hint Constructors difference : core.
@@ -78,21 +80,6 @@ Global Hint Constructors complement : core.
 
 Global Hint Unfold subset : core.
 Global Hint Unfold symmetric_difference : core.
-
-Section SubsetLemmas.
-
-  Variable U : Type.
-
-  Lemma subset_empty_set:
-    forall (A : set U), ∅ ⊆ A.
-  Proof.
-    autounfold. contradiction.
-  Qed.
-
-  Global Instance subset_preorder: PreOrder (@subset U).
-  Proof. constructor; auto. Qed.
-
-End SubsetLemmas.
 
 Section SetEquality.
 
@@ -138,136 +125,6 @@ Infix "==" := eq (at level 70).
 
 Global Hint Unfold eq : core.
 
-Section Basics.
-
-  Variable U : Type.
-
-  Lemma intersection_comm:
-    forall (A B : set U), A ⋂ B == B ⋂ A.
-  Proof.
-    split; autounfold; intros; inv H; auto.
-  Qed.
-
-  Lemma intersection_assoc:
-    forall (A B C : set U), (A ⋂ B) ⋂ C == A ⋂ (B ⋂ C).
-  Proof.
-    autounfold; split; intros; inv H.
-    - inv H0; auto.
-    - inv H1; auto.
-  Qed.
-
-  Lemma union_comm:
-    forall (A B : set U), A ⋃ B == B ⋃ A.
-  Proof.
-    autounfold; split; intros; inv H; auto.
-  Qed.
-
-  Lemma union_assoc:
-    forall (A B C : set U), (A ⋃ B) ⋃ C == A ⋃ (B ⋃ C).
-  Proof.
-    autounfold; split; intros; inv H; auto; inv H0; auto.
-  Qed.
-
-  Lemma intersection_dist:
-    forall (A B C : set U), A ⋂ (B ⋃ C) == (A ⋂ B) ⋃ (A ⋂ C).
-  Proof.
-    autounfold; split; intros; inv H.
-    - inv H1; auto.
-    - inv H0; auto.
-    - inv H0; auto.
-  Qed.
-
-  Lemma union_dist:
-    forall (A B C : set U), A ⋃ (B ⋂ C) == (A ⋃ B) ⋂ (A ⋃ C).
-  Proof.
-    autounfold; split; intros; inv H; auto.
-    - inv H0; auto.
-    - inv H0; auto. inv H1; auto.
-  Qed.
-
-  Lemma de_morgan_union : forall (A B : set U),
-    complement (A ⋃ B) == (complement A) ⋂ (complement B).
-  Proof.
-    autounfold; split; intros; inv H; clear H.
-    - auto 6.
-    - inv H0. inv H1.
-      apply complement_intro. intros contra. inv contra; auto.
-  Qed.
-
-  Lemma de_morgan_intersection:
-    forall (A B : set U),
-    complement (A ⋂ B) == (complement A) ⋃ (complement B).
-  Proof.
-    autounfold; split; intros; inv H; clear H.
-    - destruct (element_decidable x A), (element_decidable x B); auto.
-      exfalso. eauto.
-    - apply complement_intro. intros contra.
-      inv contra. inv H0. contradiction.
-    - apply complement_intro. intros contra.
-      inv contra. inv H0. contradiction.
-  Qed.
-
-End Basics.
-
-Section SymmetricDifference.
-
-  Variable U : Type.
-
-  Lemma symmetric_difference_id:
-    forall (A : set U), A △ ∅ == A.
-  Proof.
-    autounfold; split; intros.
-    - inv H; inv H0; intuition. inv H2.
-    - apply union_introl.
-      apply difference_intro; split; auto.
-      intros contra. inv contra.
-  Qed.
-
-  Lemma symmetric_difference_self:
-    forall (A : set U), A △ A == ∅.
-  Proof.
-    autounfold; split; intros.
-    - inv H; destruct H0; destruct H0; contradiction.
-    - inv H.
-  Qed.
-
-  Lemma symmetric_difference_comm:
-    forall (A B : set U), A △ B == B △ A.
-  Proof.
-    autounfold; split; intros; inv H; auto.
-  Qed.
-
-  Lemma symmetric_difference_assoc:
-    forall (A B C : set U), A △ (B △ C) == (A △ B) △ C.
-  Proof.
-    autounfold; split; intros;
-    inv H; clear H; destruct H0; inv H; clear H.
-    - destruct (element_decidable x B).
-      + apply union_intror, difference_intro; split.
-        destruct (element_decidable x C); auto.
-        * exfalso. auto.
-        * intros contra. inv contra; inv H2; destruct H3; contradiction.
-      + apply union_introl, difference_intro; split.
-        destruct (element_decidable x C); auto. intros Hc. auto.
-    - inv H0; clear H0; inv H; clear H; destruct H0.
-      + apply union_introl, difference_intro; split; auto.
-      + apply union_intror,difference_intro; split; auto.
-        intros contra; inv contra; inv H2; destruct H3; contradiction.
-    - inv H0; inv H; destruct H2; clear H H0.
-      + apply union_introl, difference_intro; split; auto.
-        intros contra. inv contra; inv H; destruct H0; contradiction.
-      + apply union_intror, difference_intro; split; auto.
-    - destruct (element_decidable x B).
-      + apply union_introl, difference_intro; split.
-        destruct (element_decidable x A); auto.
-        * exfalso. auto.
-        * intros contra. inv contra; inv H2; destruct H3; contradiction.
-      + apply union_intror, difference_intro; split.
-        destruct (element_decidable x A); auto. intros contra. auto.
-  Qed.
-
-End SymmetricDifference.
-
 Section Rewriting.
 
   Variable U : Type.
@@ -310,6 +167,349 @@ Section Rewriting.
   Qed.
 
 End Rewriting.
+
+Section Basics.
+
+  Variable U : Type.
+
+  (* Intersection Lemmas *)
+  Lemma intersection_empty:
+    forall (A : set U), A ⋂ ∅ == ∅.
+  Proof.
+    autounfold. split; intros.
+    - inv H. inv H1.
+    - inv H.
+  Qed.
+
+  Lemma intersection_full_set:
+    forall (A : set U), A ⋂ full_set == A.
+  Proof.
+    autounfold; split; intros; auto.
+    inv H; auto.
+  Qed.
+
+  Lemma intersection_self:
+    forall (A : set U), A ⋂ A == A.
+  Proof.
+    autounfold; split; intros; eauto.
+    inv H; eauto.
+  Qed.
+
+  Lemma intersection_complement:
+    forall (A : set U), A ⋂ (complement A) == ∅.
+  Proof.
+    autounfold; split; intros; inv H; inv H1; contradiction.
+  Qed.
+
+  Lemma intersection_comm:
+    forall (A B : set U), A ⋂ B == B ⋂ A.
+  Proof.
+    split; autounfold; intros; inv H; auto.
+  Qed.
+
+  Lemma intersection_assoc:
+    forall (A B C : set U), (A ⋂ B) ⋂ C == A ⋂ (B ⋂ C).
+  Proof.
+    autounfold; split; intros; inv H.
+    - inv H0; auto.
+    - inv H1; auto.
+  Qed.
+
+  (* Union Lemmas *)
+  Lemma union_empty:
+    forall (A : set U), A ⋃ ∅ == A.
+  Proof.
+    autounfold; split; intros; auto.
+    inv H; auto. inv H0.
+  Qed.
+
+  Lemma union_full_set:
+    forall (A : set U), A ⋃ full_set == full_set.
+  Proof. auto. Qed.
+
+  Lemma union_self:
+    forall (A : set U), A ⋃ A == A.
+  Proof.
+    autounfold; split; intros; auto. inv H; auto.
+  Qed.
+
+  Lemma union_complement:
+    forall (A : set U), A ⋃ (complement A) == full_set.
+  Proof.
+    autounfold; split; intros; auto.
+    destruct (element_decidable x A); eauto.
+  Qed.
+
+  Lemma union_comm:
+    forall (A B : set U), A ⋃ B == B ⋃ A.
+  Proof.
+    autounfold; split; intros; inv H; auto.
+  Qed.
+
+  Lemma union_assoc:
+    forall (A B C : set U), (A ⋃ B) ⋃ C == A ⋃ (B ⋃ C).
+  Proof.
+    autounfold; split; intros; inv H; auto; inv H0; auto.
+  Qed.
+
+  (* Distributive Laws *)
+  Lemma intersection_dist_left:
+    forall (A B C : set U), A ⋂ (B ⋃ C) == (A ⋂ B) ⋃ (A ⋂ C).
+  Proof.
+    autounfold; split; intros; inv H.
+    - inv H1; auto.
+    - inv H0; auto.
+    - inv H0; auto.
+  Qed.
+
+  Lemma intersection_dist_right:
+    forall (A B C : set U), (A ⋃ B) ⋂ C  == (A ⋂ C) ⋃ (B ⋂ C).
+  Proof.
+    intros.
+    rewrite intersection_comm.
+    rewrite intersection_dist_left.
+    rewrite (intersection_comm C A).
+    rewrite (intersection_comm C B).
+    auto.
+  Qed.
+
+  Lemma union_dist_left:
+    forall (A B C : set U), A ⋃ (B ⋂ C) == (A ⋃ B) ⋂ (A ⋃ C).
+  Proof.
+    autounfold; split; intros; inv H; auto.
+    - inv H0; auto.
+    - inv H0; auto. inv H1; auto.
+  Qed.
+
+  Lemma union_dist_right:
+    forall (A B C : set U), (A ⋂ B) ⋃ C == (A ⋃ C) ⋂ (B ⋃ C).
+  Proof.
+    intros.
+    rewrite union_comm.
+    rewrite union_dist_left.
+    rewrite (union_comm C A).
+    rewrite (union_comm C B).
+    auto.
+  Qed.
+
+  (* De Morgan Laws *)
+  Lemma de_morgan_union:
+    forall (A B : set U),
+      complement (A ⋃ B) == (complement A) ⋂ (complement B).
+  Proof.
+    autounfold; split; intros; inv H; clear H.
+    - auto 6.
+    - inv H0. inv H1.
+      apply complement_intro. intros contra. inv contra; auto.
+  Qed.
+
+  Lemma de_morgan_intersection:
+    forall (A B : set U),
+      complement (A ⋂ B) == (complement A) ⋃ (complement B).
+  Proof.
+    autounfold; split; intros; inv H; clear H.
+    - destruct (element_decidable x A), (element_decidable x B); auto.
+      exfalso. eauto.
+    - apply complement_intro. intros contra.
+      inv contra. inv H0. contradiction.
+    - apply complement_intro. intros contra.
+      inv contra. inv H0. contradiction.
+  Qed.
+
+  (* Complement Laws *)
+  Lemma complement_empty:
+    (@complement U ∅) == full_set.
+  Proof.
+    autounfold; split; intros; auto.
+    apply complement_intro. intros contra. inv contra.
+  Qed.
+
+  Lemma complement_full_set:
+    (@complement U full_set) == ∅.
+  Proof.
+    autounfold; split; intros.
+    - inv H. exfalso. apply H0. auto.
+    - inv H.
+  Qed.
+
+  Lemma complement_complement:
+    forall (A : set U), complement (complement A) == A.
+  Proof.
+    autounfold; split; intros.
+    - destruct (element_decidable x A); auto.
+      inv H. exfalso. auto.
+    - apply complement_intro. intros contra.
+      inv contra. contradiction.
+  Qed.
+
+  (* Difference Laws *)
+  Lemma difference_as_intersection:
+    forall (A B : set U), A \ B == A ⋂ (complement B).
+  Proof.
+    autounfold; split; intros.
+    - split; inv H; destruct H0; eauto.
+    - inv H. inv H1. eauto.
+  Qed.
+
+  Corollary difference_empty:
+    forall (A : set U), A \ ∅ == A.
+  Proof.
+    intros.
+    rewrite difference_as_intersection.
+    rewrite complement_empty.
+    rewrite intersection_full_set.
+    reflexivity.
+  Qed.
+
+  Corollary difference_from_empty:
+    forall (A : set U), ∅ \ A == ∅.
+  Proof.
+    intros.
+    rewrite difference_as_intersection.
+    rewrite intersection_comm.
+    rewrite intersection_empty.
+    reflexivity.
+  Qed.
+
+  Corollary difference_self:
+    forall (A : set U), A \ A == ∅.
+  Proof.
+    intros.
+    rewrite difference_as_intersection.
+    rewrite intersection_complement.
+    reflexivity.
+  Qed.
+
+End Basics.
+
+Section SubsetLemmas.
+
+  Variable U : Type.
+
+  Global Instance subset_preorder: PreOrder (@subset U).
+  Proof. constructor; auto. Qed.
+
+  Lemma subset_empty_set:
+    forall (A : set U), ∅ ⊆ A.
+  Proof.
+    autounfold. contradiction.
+  Qed.
+
+  Lemma subset_intersection:
+    forall (A B : set U), A ⊆ B <-> A ⋂ B == A.
+  Proof.
+    autounfold; split; intros.
+    - split; intros; auto. inv H0. auto.
+    - destruct H. apply H1 in H0. destruct H0. auto.
+  Qed.
+
+  Lemma subset_union:
+    forall (A B : set U), A ⊆ B <-> A ⋃ B == B.
+  Proof.
+    autounfold; split; intros.
+    - split; intros; auto. inv H0; auto.
+    - destruct H. auto.
+  Qed.
+
+  Lemma subset_difference:
+    forall (A B : set U), A ⊆ B <-> A \ B == ∅.
+  Proof.
+    autounfold; split; intros.
+    - split; intros; try inv H0.
+      destruct H1 as [H1 H2]. exfalso. auto.
+    - destruct H, (element_decidable x B); auto.
+      assert (x ∈ ∅) by auto. inv H3.
+  Qed.
+
+  Corollary subset_intersection_complement:
+    forall (A B : set U), A ⊆ B <-> A ⋂ (complement B) == ∅.
+  Proof.
+    intros. rewrite <- difference_as_intersection.
+    apply subset_difference.
+  Qed.
+
+  Lemma subset_complement_subset:
+    forall (A B : set U),
+      A ⊆ B <-> (complement B) ⊆ (complement A).
+  Proof.
+    autounfold; split; intros.
+    - inv H0. auto.
+    - destruct (element_decidable x B); auto.
+      assert (x ∈ complement B) by auto.
+      apply H in H2. inv H2. contradiction.
+  Qed.
+
+  Corollary subset_complement_difference:
+    forall (A B : set U),
+      A ⊆ B <-> (complement B) \ (complement A) == ∅.
+  Proof.
+    etransitivity.
+    apply subset_complement_subset.
+    apply subset_difference.
+  Qed.
+
+End SubsetLemmas.
+
+Section SymmetricDifference.
+
+  Variable U : Type.
+
+  Lemma symmetric_difference_id:
+    forall (A : set U), A △ ∅ == A.
+  Proof.
+    intros. unfold symmetric_difference.
+    rewrite difference_empty.
+    rewrite difference_from_empty.
+    apply union_empty.
+  Qed.
+
+  Lemma symmetric_difference_self:
+    forall (A : set U), A △ A == ∅.
+  Proof.
+    intros. unfold symmetric_difference.
+    rewrite difference_self.
+    rewrite union_empty.
+    reflexivity.
+  Qed.
+
+  Lemma symmetric_difference_comm:
+    forall (A B : set U), A △ B == B △ A.
+  Proof.
+    intros. unfold symmetric_difference.
+    rewrite union_comm.
+    reflexivity.
+  Qed.
+
+  Lemma symmetric_difference_assoc:
+    forall (A B C : set U), A △ (B △ C) == (A △ B) △ C.
+  Proof.
+    autounfold; split; intros;
+    inv H; clear H; destruct H0; inv H; clear H.
+    - destruct (element_decidable x B).
+      + apply union_intror, difference_intro; split.
+        destruct (element_decidable x C); auto.
+        * exfalso. auto.
+        * intros contra. inv contra; inv H2; destruct H3; contradiction.
+      + apply union_introl, difference_intro; split.
+        destruct (element_decidable x C); auto. intros Hc. auto.
+    - inv H0; clear H0; inv H; clear H; destruct H0.
+      + apply union_introl, difference_intro; split; auto.
+      + apply union_intror,difference_intro; split; auto.
+        intros contra; inv contra; inv H2; destruct H3; contradiction.
+    - inv H0; inv H; destruct H2; clear H H0.
+      + apply union_introl, difference_intro; split; auto.
+        intros contra. inv contra; inv H; destruct H0; contradiction.
+      + apply union_intror, difference_intro; split; auto.
+    - destruct (element_decidable x B).
+      + apply union_introl, difference_intro; split.
+        destruct (element_decidable x A); auto.
+        * exfalso. auto.
+        * intros contra. inv contra; inv H2; destruct H3; contradiction.
+      + apply union_intror, difference_intro; split.
+        destruct (element_decidable x A); auto. intros contra. auto.
+  Qed.
+
+End SymmetricDifference.
 
 Section PowerSet.
 
